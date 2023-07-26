b4aa1d2db4b78dc263c7110117f8ba4d76f06ccf
Sirius <siriusproger@gmail.com>
2023-07-26 20:31:35 +0500

    added firstly views

--- dj_ihona_task/blog/views.py
+++ dj_ihona_task/blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# views.py
+
+from rest_framework import viewsets
+from .models import Tag, Post, Question, UserQAProfile
+from .serializers import TagSerializer, PostSerializer, QuestionSerializer, UserQAProfileSerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class UserQAProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserQAProfile.objects.all()
+    serializer_class = UserQAProfileSerializer
