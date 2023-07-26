543ce5bffab8963b41d10f34143427d96b94348d
Sirius <siriusproger@gmail.com>
2023-07-26 21:52:27 +0500

    Added execut file for run creating super suer automaticly

---
+++ dj_ihona_task/entryscript.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Wait for the database to be ready (if you are using a database)
+# Here's an example for PostgreSQL:
+# while ! nc -z db 5432; do
+#   sleep 0.5
+# done
+
+# Run migrations
+python3 manage.py migrate
+
+# Create default user
+python3 manage.py create_default_user
+
+# Start your application
+exec "$@"
