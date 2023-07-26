1867fa5cb71f13aa5bdf7c155cc58b49fb800c35
Sirius <siriusproger@gmail.com>
2023-07-26 20:26:55 +0500

    git added models and registered models into admin panel

--- dj_ihona_task/blog/admin.py
+++ dj_ihona_task/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tag, Post, Image, Question, UserQAProfile, UserResponse
+
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Image)
+admin.site.register(Question)
+admin.site.register(UserQAProfile)
+admin.site.register(UserResponse)
--- dj_ihona_task/blog/models.py
+++ dj_ihona_task/blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    images = models.ManyToManyField('Image', blank=True, related_name="post_images")
+    content = models.TextField()
+    modification_date = models.DateTimeField(auto_now=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='posts/images/')
+    post = models.ForeignKey(Post, related_name='image_post', on_delete=models.CASCADE)
+
+class Question(models.Model):
+    post = models.ForeignKey(Post, related_name='polls', on_delete=models.CASCADE)
+    question_text = models.TextField()
+    choice_1 = models.CharField(max_length=255)
+    choice_2 = models.CharField(max_length=255)
+    choice_3 = models.CharField(max_length=255)
+    choice_4 = models.CharField(max_length=255)
+
+class UserQAProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    answered_questions = models.ManyToManyField(Question, through='UserResponse')
+
+class UserResponse(models.Model):
+    profile = models.ForeignKey(UserQAProfile, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    selected_answer = models.CharField(max_length=255)
