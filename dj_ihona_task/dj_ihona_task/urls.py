ba85a6990313066acce5f7c83cc240720ff5eecb
Sirius <siriusproger@gmail.com>
2023-07-26 20:40:37 +0500

    changed router urls

--- dj_ihona_task/dj_ihona_task/urls.py
+++ dj_ihona_task/dj_ihona_task/urls.py
@@ -5,6 +5,16 @@ from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from blog.views import TagViewSet, PostViewSet, QuestionViewSet, UserQAProfileViewSet
+
+router = DefaultRouter()
+
+router.register(r'tags', TagViewSet)
+router.register(r'posts', PostViewSet)
+router.register(r'questions', QuestionViewSet)
+router.register(r'userprofiles', UserQAProfileViewSet)
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -16,8 +26,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
-
