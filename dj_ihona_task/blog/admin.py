4c80fffa77c538ed10b2b6a061b9129a71a940a8
Sirius <siriusproger@gmail.com>
2023-07-26 21:17:18 +0500

    added TabularInline to show question adn answer of user into its page

--- dj_ihona_task/blog/admin.py
+++ dj_ihona_task/blog/admin.py
@@ -7,5 +7,16 @@ admin.site.register(Tag)
 admin.site.register(Post)
 admin.site.register(Image)
 admin.site.register(Question)
-admin.site.register(UserQAProfile)
 admin.site.register(UserResponse)
+
+
+class UserResponseInline(admin.TabularInline):  # or admin.StackedInline if you prefer that style
+    model = UserResponse
+    extra = 0  # This means no empty forms will be shown; you can adjust as necessary
+    readonly_fields = ['question', 'selected_answer'] 
+
+
+class UserQAProfileAdmin(admin.ModelAdmin):
+    inlines = [UserResponseInline]
+
+admin.site.register(UserQAProfile, UserQAProfileAdmin)
